28279ed58a6032950105927bc431a145651c3edd
Alfonso Boza <alfonso@al.fon.so>
2013-05-29 16:18:10 -0400

    Source EC2 env only if on OS X

--- ec2/env.zsh
+++ ec2/env.zsh
@@ -1,6 +1,8 @@
-# Copied from `brew info ec2-api-tools` and `brew info ec2-ami-tools`
-export JAVA_HOME="$(/usr/libexec/java_home)"
-export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)"
-export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
-export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
-export EC2_AMITOOL_HOME="/usr/local/Library/LinkedKegs/ec2-ami-tools/jars"
+if [[ $(uname) == 'Darwin' ]]; then
+  # Copied from `brew info ec2-api-tools` and `brew info ec2-ami-tools`
+  export JAVA_HOME="$(/usr/libexec/java_home)"
+  export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)"
+  export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
+  export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
+  export EC2_AMITOOL_HOME="/usr/local/Library/LinkedKegs/ec2-ami-tools/jars"
+fi
